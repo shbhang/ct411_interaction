d0059a3908f833fcdefc1b8394313abf5472e568
shbhang <sunghyun_bang@fitnyc.edu>
2017-09-05 19:23:20 -0400

    week1_hw_1st version

---
+++ week1_inclass/sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+createCanvas(800,600);
+    
+    
+    
+}
+function draw(){
+background(255,204,0);
+fill(50, 101, 214);
+noStroke();
+rect(350, 200, 60, 60, 30, 30, 5, 5);
+noFill();
+    //fill(244, 233, 186);   
+fill(244, 233, 186);
+
+ellipse(370, 150, 100, 100);    
+
+noFill();
+stroke(0);
+curve(10, 30, 300, 180, 50, 184, 373, 361);
+line(20, 160, 45, 15,);
+    
+}
