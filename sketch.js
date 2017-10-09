66c2e935385e15812ff133f561aee829d9e1e399
shbhang <sunghyun_bang@fitnyc.edu>
2017-10-09 15:12:16 -0400

    w5_hw

--- sketch.js
+++ sketch.js
@@ -1,41 +1,40 @@
-var img;
-var img2;
-var isClicked = false;
-
-
-
-function preload(){
-    img = loadImage("sun.png");
-    img2 = loadimage("moon.png");
-   
-    
-}
+var rectX = 300;
+var rectY = 300;
+var rectSize = 400;
+var rectTop = rectY;
+var rectBottom = rectY + rectSize;
+var rectLeft = rectX;
+var rectRight = rectX + rectSize;
+var colR;
 
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     
-    
 }
-
 
 function draw(){
     
-    background(39,170,225);
-    imageMode(CENTER);
-    image(img, width/2, height/2, 200, 200);
-   
-       
-if (mouseIsPressed){
+    background(0);
+  
+    if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom){
+        colR = 249;  
+                
+                textSize(100);
+                textAlign(CENTER);
+                text("CODING IS", 700, 150);
+                textAlign(CENTER);
+                text("SO MUCH FUN", 900, 250);
+                textAlign(CENTER);
+                text("JOYFUL", 1035, 350);
         
-   background(0);          
-
-  } else {
-      
-    background(39,170,225);
-    
-  }
-
+    }else{
+        colR = 0;   
     }
-
-
+        fill(colR, 0, 0);
+        rect(rectX, rectY, rectSize, rectSize);
+    
+    
+    
+    
+}
